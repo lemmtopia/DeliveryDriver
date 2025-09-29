be8ccd8bd800667e4ee43cd2beabe09ebf1971a8
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2025-09-29 10:52:28 -0300

    Add camera damping

--- DeliveryDriver/Assets/Scripts/FollowCamera.cs
+++ DeliveryDriver/Assets/Scripts/FollowCamera.cs
@@ -6,12 +6,13 @@ public class FollowCamera : MonoBehaviour
 {
     // The camera's position should be the same as the driver's position.
     [SerializeField] private GameObject follow;
+    [SerializeField] private float damping = 25;
 
     void Update()
     {
         Vector3 targetPos = follow.transform.position;
         targetPos.z = transform.position.z; // Use my Z instead of the follow's Z.
 
-        transform.position = targetPos;
+        transform.position = Vector3.Lerp(transform.position, targetPos, damping * Time.deltaTime);
     }
 }
